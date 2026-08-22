630a04f03291c772ee8feb40602e302c1c143719
DewaSRY <sdewa6645@gmail.com>
2026-08-22 22:36:29 +0700

    ini:add migration up and down, and make file helper

--- apps/core-service/cmd/migration/main.go
+++ apps/core-service/cmd/migration/main.go
@@ -7,13 +7,49 @@ import (
 	"os"
 )
 
+var DB_URI = "postgres://simple_bank:password@localhost:5433/simple_bank?sslmode=disable"
+
 func createMigrationFile(migrationName string) {
 	cmd := exec.Command("bash", "-c", fmt.Sprintf("migrate create -ext sql -dir db/migrations -seq %s", migrationName))
 	output, err := cmd.CombinedOutput()
 	if err != nil {
 		fmt.Println("Error:", err)
+		fmt.Println("Output:", string(output))
 		return
 	}
+
+	fmt.Println(string(output))
+}
+
+func upMigration() {
+
+	cmd := exec.Command("bash", "-c", fmt.Sprintf("migrate -path db/migrations -database %s up", DB_URI))
+	output, err := cmd.CombinedOutput()
+	fmt.Println("Running migration up...")
+	if err != nil {
+		fmt.Println("Migration up failed.")
+		fmt.Println("Error:", err)
+		fmt.Println("Output:", string(output))
+		return
+	}
+
+	fmt.Println("Migration up completed.")
+
+	fmt.Println(string(output))
+}
+
+func downMigration() {
+	cmd := exec.Command("bash", "-c", fmt.Sprintf("migrate -path db/migrations -database %s down", DB_URI))
+	output, err := cmd.CombinedOutput()
+	fmt.Println("Running migration down...")
+	if err != nil {
+		fmt.Println("Migration down failed.")
+		fmt.Println("Error:", err)
+		fmt.Println("Output:", string(output))
+		return
+	}
+
+	fmt.Println("Migration down completed.")
 
 	fmt.Println(string(output))
 }
@@ -35,7 +71,10 @@ func main() {
 		}
 
 		createMigrationFile(args[1])
-
+	case "up":
+		upMigration()
+	case "down":
+		downMigration()
 	default:
 		fmt.Println("Unknown command:", args[0])
 	}
---
+++ apps/core-service/db/migrations/000001_init-transaction-feature.up.sql
@@ -0,0 +1,56 @@
+
+CREATE TABLE IF NOT EXISTS "accounts"  (
+    id BIGSERIAL PRIMARY KEY,
+    "owner" VARCHAR(255) NOT NULL,
+    "balance" BIGINT NOT NULL,
+    "currency" VARCHAR(3) NOT NULL,
+    "created_at" timestamptz  NOT NULL DEFAULT now(),
+    "updated_at" timestamptz  NOT NULL DEFAULT now()
+);
+
+
+CREATE TABLE IF NOT EXISTS "transfers" (
+    id BIGSERIAL PRIMARY KEY,
+    from_account_id BIGINT NOT NULL,
+    to_account_id BIGINT NOT NULL,
+    amount BIGINT NOT NULL,
+    created_at timestamptz  NOT NULL DEFAULT now()
+);
+
+
+CREATE TABLE IF NOT EXISTS "entries" (
+    id BIGSERIAL PRIMARY KEY,
+    account_id BIGINT NOT NULL,
+    type VARCHAR(50) NOT NULL,
+    amount BIGINT NOT NULL,
+    created_at timestamptz  NOT NULL DEFAULT now()
+);
+
+
+
+ALTER TABLE "entries" ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id");
+
+ALTER TABLE "transfers" ADD FOREIGN KEY ("from_account_id") REFERENCES "accounts" ("id");
+ALTER TABLE "transfers" ADD FOREIGN KEY ("to_account_id") REFERENCES "accounts" ("id");
+
+
+CREATE INDEX ON "accounts" ("owner");
+
+CREATE INDEX ON "entries" ("account_id");
+
+CREATE INDEX ON "transfers" ("from_account_id");
+
+CREATE INDEX ON "transfers" ("to_account_id");
+
+
+
+COMMENT ON TABLE "accounts" IS 'Table to store account information';
+COMMENT ON COLUMN "accounts"."balance" IS 'Balance cannot be negative, it is the amount of money in the account';
+
+COMMENT ON TABLE "entries" IS 'Table to store account entries for each transaction';
+COMMENT ON COLUMN "entries"."amount" IS 'Amount cannot be negative, it is the amount of money added or subtracted from the account';
+COMMENT ON COLUMN "entries"."type" IS 'Type use to define the purpose of the entry';
+
+COMMENT ON TABLE "transfers" IS 'Table to store transfer transactions between accounts';    
+COMMENT ON COLUMN "transfers"."amount" IS 'Amount cannot be negative, it is the amount of money transferred from one account to another';
+
